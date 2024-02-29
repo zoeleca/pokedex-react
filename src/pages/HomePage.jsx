12463d90461efafa88701ade7e65f55f55b81ea6
zoeleca <zoennts@gmail.com>
2024-02-29 11:45:02 +0100

    pokemon-quizz route fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
     return (
         <>
-        <div className="home-container">
+            <div className="home-container">
                 <div className="logo">
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="pokemonLogo" />
                 </div>
-            <div className="title">
-            <h1>Choose your adventure : </h1>
-            </div>
-            <div className="links-group">
-            <a href="/pokedex"><button className="home-btn">Pokedex</button></a>
-            <a href="/pokedex-quiz"><button className="home-btn">Test tes connaissances</button></a>
-            </div>
+                <div className="title">
+                    <h1>Choose your adventure : </h1>
+                </div>
+                <div className="links-group">
+                    <Link to="/pokedex">
+                        <button className="home-btn">Pokedex</button>
+                    </Link>
+                    <Link to="/pokemon-quiz">
+                        <button className="home-btn">Test tes connaissances</button>
+                    </Link>
+                </div>
             </div>
         </>
     )
