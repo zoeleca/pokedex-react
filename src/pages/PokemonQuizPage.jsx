7d78fb1c068e4676562ad853a87fb1f2e22c68a2
zoeleca <zoennts@gmail.com>
2024-03-01 12:14:06 +0100

    pokemon quizz updated + style css

--- src/pages/PokemonQuizPage.jsx
+++ src/pages/PokemonQuizPage.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import axios from "axios";
+import Quizz from "../components/Quizz";
 
 const PokemonQuizPage = () => {
+    const [pokeQuizzData, setPokeQuizzData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [randomUrl, setRandomUrl] = useState('');
+
+    const fetchRandomPokemon = async () => {
+        setIsLoading(true);
+        try {
+            const random = Math.floor(Math.random() * (151 - 1) + 1);
+            console.log("random : " , random)
+            const randomUrl = `https://pokeapi.co/api/v2/pokemon/${random}`;
+            setRandomUrl(randomUrl);
+            const res = await axios.get(randomUrl);
+            console.log("Response data:", res.data);
+            setPokeQuizzData(res.data);
+            setError(null); // Reset error state if request succeeds
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        console.warn("useffect launch twice")
+        fetchRandomPokemon();
+    }, [1]); // Empty dependency array means the effect runs only once on mount
+
     return (
-        <>
-        <div className="home-container">
-            <div className="logo">
+        <div className="pokemonquizz_container">
             <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="pokemonLogo"/>
-            </div>
-            <div className="title">
-            <h1>Choose your adventure : </h1>
-            </div>
-            <div className="links-group">
-                <button className="home-btn"><a href="/pokedex">Pokedex</a></button>
-                <button className="home-btn">Cartes</button>
-                <button className="home-btn"><a href="/pokedex-quiz">Test tes connaissances</a></button>
-            </div>
-            </div>
-        </>
-    )
-}
+                className="logo-img"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png"
+                alt="PokemonLogo"/>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {!isLoading && !error && <Quizz quizzData={pokeQuizzData} updateRandomUrl={fetchRandomPokemon} />}
+        </div>
+    );
+};
 
 export default PokemonQuizPage;
